aa97847a851cbd4c22b1c9b508e77c9a78247511
Sebastian Montenegro <66041836+sebmontenegro@users.noreply.github.com>
2020-08-24 12:51:04 -0500

    Add files via upload

---
+++ preload.R
@@ -0,0 +1,9 @@
+preload <- function(x){
+  #This function evaluates Package x. If it is installed, it loads the package. If it is
+  #not, it installs it.
+  x <- as.character(x)
+  if(!require(x,character.only = TRUE)){
+    install.packages(x,repos = "https://cran.r-project.org")
+    require(x,character.only = TRUE)}
+  require(x,character.only = TRUE)
+}
